8343390edc1404c52c261f8bc89b3c7c416d2096
Aleksandar <aca801@gmail.com>
2020-08-02 18:50:29 +0200

    Add Order by checks in Specification Evaluator

--- server/Infrastructure/Data/Specifications/SpecificationEvaluator.cs
+++ server/Infrastructure/Data/Specifications/SpecificationEvaluator.cs
@@ -19,6 +19,16 @@ namespace Infrastructure.Data.Specifications
                 query = query.Where(spec.Criteria);
             }
 
+            if (spec.OrderBy != null)
+            {
+                query = query.OrderBy(spec.OrderBy);
+            }
+
+            if (spec.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(spec.OrderByDescending);
+            }
+
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
 
             // Aggregate first query for string includes
