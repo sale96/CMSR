7ce299726561de9bfeb16df8f4c20d125d0a7059
Aleksandar <aca801@gmail.com>
2020-08-02 18:57:59 +0200

    Add product id to Product data transfer object

--- server/API/Models/Dto/ProductDto.cs
+++ server/API/Models/Dto/ProductDto.cs
@@ -8,6 +8,7 @@ namespace API.Models.Dto
 {
     public class ProductDto : BaseDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
